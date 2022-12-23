fbc2841a4fa94d4f02a706e3f5fc3e77c95bfb2d
Patrick Dreker <patrick@dreker.de>
2022-12-23 17:44:47 +0100

    print internal representation (fields)

--- gpspy/__main__.py
+++ gpspy/__main__.py
@@ -5,4 +5,4 @@ with serial.Serial("/dev/ttyACM0") as ser:
     while True:
         line = ser.readline().decode("ascii")
         sentence = pynmea2.parse(line)
-        print(sentence)
+        print(sentence.fields)
