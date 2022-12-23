cc7c617389b5f456662a3119dd2713930185fb46
Patrick Dreker <patrick@dreker.de>
2022-12-23 17:26:48 +0100

    parse lines

--- gpspy/__main__.py
+++ gpspy/__main__.py
@@ -4,4 +4,5 @@ import serial
 with serial.Serial("/dev/ttyACM0") as ser:
     while True:
         line = ser.readline()
-        print(line)
+        sentence = pynmea2.parse(line)
+        print(sentence)
