176b2fe630b21beda4e4cfa867fb06c8c22bf2b3
Patrick Dreker <patrick@dreker.de>
2022-12-23 17:28:57 +0100

    decode bytearray to ascii string

--- gpspy/__main__.py
+++ gpspy/__main__.py
@@ -3,6 +3,6 @@ import serial
 
 with serial.Serial("/dev/ttyACM0") as ser:
     while True:
-        line = ser.readline()
+        line = ser.readline().decode("ascii")
         sentence = pynmea2.parse(line)
         print(sentence)
