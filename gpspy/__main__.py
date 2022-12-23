a72cababffc1378d6487e0958655dcdddcabadc2
Patrick Dreker <patrick@dreker.de>
2022-12-23 17:24:50 +0100

    actually loop...

--- gpspy/__main__.py
+++ gpspy/__main__.py
@@ -2,5 +2,6 @@ import pynmea2
 import serial
 
 with serial.Serial("/dev/ttyACM0") as ser:
-    line = ser.readline()
-    print(line)
+    while True:
+        line = ser.readline()
+        print(line)
